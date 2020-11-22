b5d9e9fb30c510f17535a1e091764d9e9427df96
nminchow <noelminchow@gmail.com>
2020-11-22 18:21:47 -0500

    add ticker and footer links

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,20 +5,26 @@
       <Definition />
     </div>
     <div class="centered ticker">
-      yo
+      <Ticker />
     </div>
+  </div>
+  <div class="centered even">
+    <a href="https://www.blockchain.com/btc/address/36dafGyDx7rsTg6TKWjnCKz6XfK3PwfCCk">36dafGyDx7rsTg6TKWjnCKz6XfK3PwfCCk</a>
+    <a href="github">Github</a>
   </div>
 </template>
 
 <script>
 import Satoshi from '@/components/Satoshi.vue';
 import Definition from '@/components/Definition.vue';
+import Ticker from '@/components/Ticker.vue';
 
 export default {
   name: 'Home',
   components: {
     Satoshi,
     Definition,
+    Ticker,
   },
 };
 </script>
@@ -43,5 +49,10 @@ export default {
   display: flex;
   flex-direction: column;
   height: 100vh;
+}
+
+.even {
+  flex-direction: column;
+  font-size: 1rem;
 }
 </style>
