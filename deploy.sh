271dd283f6b16f53ecc317917068c7f31d346cbe
nminchow <noelminchow@gmail.com>
2020-11-22 19:13:54 -0500

    set up deployment script

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+yarn run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+echo 'satoshi.cool' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:nminchow/satoshi.cool.git master:gh-pages
+
+cd -
